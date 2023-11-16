5f1399a864003c42740575adccea067204a2cecb
Nicolas <nicolas.aguilar180193@gmail.com>
2023-11-16 12:35:18 -0300

    Customer and Invoice add fields to migrations

--- database/migrations/2023_11_16_040513_create_invoices_table.php
+++ database/migrations/2023_11_16_040513_create_invoices_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->integer('customer_id');
+            $table->integer('amount');
+            $table->string('status');
+            $table->datatime('billed_dated');
+            $table->datatime('paid_dated')->nullable();
             $table->timestamps();
         });
     }
