eb33d547a397de1b1dc926fed22cf0bc93c62c4e
Nicolas <nicolas.aguilar180193@gmail.com>
2023-11-17 21:25:10 -0300

    Update customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -68,7 +68,8 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
+        return new CustomerResource($customer);
     }
 
     /**
