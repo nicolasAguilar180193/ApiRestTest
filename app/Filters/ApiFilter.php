37c3382441b16ddf819639bd707cf577bc42c397
Nicolas <nicolas.aguilar180193@gmail.com>
2023-11-23 13:23:55 -0300

    Coding standars are applied in Filters

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -6,24 +6,25 @@ use Illuminate\Http\Request;
 
 class ApiFilter
 {
-    protected $safeParams = [];
-    protected $columnMap = [];
-    protected $operatorMap = [];
+	protected $safeParams = [];
+	protected $columnMap = [];
+	protected $operatorMap = [];
 
-    public function transform(Request $request){
-        $eloQuery = [];
-        foreach($this->safeParams as $param => $operators){
-            $query = $request->query($param);
-            if(!isset($query)){
-                continue;
-            }
-            $column = $this->columnMap[$param] ?? $param;
-            foreach($operators as $operator){
-                if(isset($query[$operator])){
-                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
-                }
-            }
-        }
-        return $eloQuery;
-    }
+	public function transform(Request $request)
+	{
+		$eloQuery = [];
+		foreach ($this->safeParams as $param => $operators) {
+			$query = $request->query($param);
+			if (!isset($query)) {
+				continue;
+			}
+			$column = $this->columnMap[$param] ?? $param;
+			foreach ($operators as $operator) {
+				if (isset($query[$operator])) {
+					$eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+				}
+			}
+		}
+		return $eloQuery;
+	}
 }
--- app/Filters/InvoiceFilter.php
+++ app/Filters/InvoiceFilter.php
@@ -2,32 +2,29 @@
 
 namespace App\Filters;
 
-use Illuminate\Http\Request;
-use App\Filters\ApiFilter;
-
 class InvoiceFilter extends ApiFilter
 {
-    protected $safeParams = [
-        'customerId' => ['eq'],
-        'amount' => ['eq', 'gt', 'lt', 'gte', 'lte'],
-        'status' => ['eq', 'neq'],
-        'billedDate' => ['eq', 'gt', 'lt', 'gte', 'lte'],
-        'paidDate' => ['eq', 'gt', 'lt', 'gte', 'lte'],
+	protected $safeParams = [
+		'customerId' => ['eq'],
+		'amount' => ['eq', 'gt', 'lt', 'gte', 'lte'],
+		'status' => ['eq', 'neq'],
+		'billedDate' => ['eq', 'gt', 'lt', 'gte', 'lte'],
+		'paidDate' => ['eq', 'gt', 'lt', 'gte', 'lte'],
 
-    ];
+	];
 
-    protected $columnMap = [
-        'customerId' => 'customer_id',
-        'billedDate' => 'billed_dated',
-        'paidDate' => 'paid_dated',
-    ];
+	protected $columnMap = [
+		'customerId' => 'customer_id',
+		'billedDate' => 'billed_dated',
+		'paidDate' => 'paid_dated',
+	];
 
-    protected $operatorMap = [
-        'eq' => '=',
-        'lt' => '<',
-        'lte' => '<=',
-        'gt' => '>',
-        'gte' => '>=',
-        'neq' => '!='
-    ];
+	protected $operatorMap = [
+		'eq' => '=',
+		'lt' => '<',
+		'lte' => '<=',
+		'gt' => '>',
+		'gte' => '>=',
+		'neq' => '!=',
+	];
 }
