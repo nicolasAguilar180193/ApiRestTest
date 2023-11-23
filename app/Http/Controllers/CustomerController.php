1de6b9671e93a82217ce20ebef4350abce8b067b
Nicolas <nicolas.aguilar180193@gmail.com>
2023-11-23 18:27:43 -0300

    Apply standars in Controllers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,81 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Filters\CustomerFilter;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\CustomerCollection;
 use App\Http\Resources\CustomerResource;
-use App\Filters\CustomerFilter;
-use Illuminate\Http\Request;
 use App\Models\Customer;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
-    {
-        $filter = new CustomerFilter();
-        $queryItems = $filter->transform($request);
-        $includeInvoices = $request->query('includeInvoices');
-        $customer = Customer::where($queryItems);
-        if($includeInvoices) {
-            $customer = $customer->with('invoices');
-        }
-        return new CustomerCollection($customer->paginate()->appends($request->query()));
-    }
+	/**
+	 * Display a listing of the resource.
+	 */
+	public function index(Request $request)
+	{
+		$filter = new CustomerFilter();
+		$queryItems = $filter->transform($request);
+		$includeInvoices = $request->query('includeInvoices');
+		$customer = Customer::where($queryItems);
+		if ($includeInvoices) {
+			$customer = $customer->with('invoices');
+		}
+		return new CustomerCollection($customer->paginate()->appends($request->query()));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreCustomerRequest $request)
-    {
-        return new CustomerResource(Customer::create($request->all()));
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(StoreCustomerRequest $request)
+	{
+		return new CustomerResource(Customer::create($request->all()));
+	}
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Customer $customer)
-    {
-        $includeInvoices = request()->query('includeInvoices');
-        if($includeInvoices) {
-            return new CustomerResource($customer->loadMissing('invoices'));
-        }
-        return new CustomerResource($customer);
-    }
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(Customer $customer)
+	{
+		$includeInvoices = request()->query('includeInvoices');
+		if ($includeInvoices) {
+			return new CustomerResource($customer->loadMissing('invoices'));
+		}
+		return new CustomerResource($customer);
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Customer $customer)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 */
+	public function edit(Customer $customer)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateCustomerRequest $request, Customer $customer)
-    {
-        $customer->update($request->all());
-        return new CustomerResource($customer);
-    }
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(UpdateCustomerRequest $request, Customer $customer)
+	{
+		$customer->update($request->all());
+		return new CustomerResource($customer);
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Customer $customer)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(Customer $customer)
+	{
+		//
+	}
 }
--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,87 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use App\Filters\InvoiceFilter;
+use App\Http\Requests\BulkStoreInvoiceRequest;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Resources\InvoiceCollection;
-use App\Http\Requests\BulkStoreInvoiceRequest;
-use App\Filters\InvoiceFilter;
-use Illuminate\Http\Request;
 use App\Models\Invoice;
+use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
-    {
-        $filter = new InvoiceFilter();
-        $queryItems = $filter->transform($request);
+	/**
+	 * Display a listing of the resource.
+	 */
+	public function index(Request $request)
+	{
+		$filter = new InvoiceFilter();
+		$queryItems = $filter->transform($request);
 
-        if(count($queryItems) == 0){
-            return new InvoiceCollection(Invoice::paginate());
-        } else {
-            $invoice = Invoice::where($queryItems)->paginate();
-            return new InvoiceCollection($invoice->appends($request->query()));
-        }
-    }
+		if (count($queryItems) === 0) {
+			return new InvoiceCollection(Invoice::paginate());
+		}
+		$invoice = Invoice::where($queryItems)->paginate();
+		return new InvoiceCollection($invoice->appends($request->query()));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreInvoiceRequest $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(StoreInvoiceRequest $request)
+	{
+		//
+	}
 
-    public function bulkStore(BulkStoreInvoiceRequest $request)
-    {
-        $bulk = collect($request->all())->map(function ($arr, $key) {
-            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
-        });
-        Invoice::insert($bulk->toArray());
-        return response()->json(['success' => true]);
-    }
+	public function bulkStore(BulkStoreInvoiceRequest $request)
+	{
+		$bulk = collect($request->all())->map(function ($arr, $key) {
+			return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+		});
+		Invoice::insert($bulk->toArray());
+		return response()->json(['success' => true]);
+	}
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Invoice $invoice)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(Invoice $invoice)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Invoice $invoice)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 */
+	public function edit(Invoice $invoice)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateInvoiceRequest $request, Invoice $invoice)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(UpdateInvoiceRequest $request, Invoice $invoice)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Invoice $invoice)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(Invoice $invoice)
+	{
+		//
+	}
 }
