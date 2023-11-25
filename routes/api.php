38b609f821ffcd917cdd14c33d8c1c388683ccf7
Nicolas <nicolas.aguilar180193@gmail.com>
2023-11-25 11:36:24 -0300

    Login and logout user routes

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function () {
+	Route::post('user/register', ['uses' => 'UserController@register']);
+	Route::post('user/login', ['uses' => 'UserController@login']);
 });
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers', 'middleware' => ['auth:sanctum']], function () {
-    Route::apiResource('customers', CustomerController::class);
-    Route::apiResource('invoices', InvoiceController::class);
-    Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
-    Route::post('user/register', ['uses' => 'UserController@register']);
+	Route::apiResource('customers', CustomerController::class);
+	Route::apiResource('invoices', InvoiceController::class);
+	Route::post('invoices/bulk', ['uses' => 'InvoiceController@bulkStore']);
+	Route::post('user/logout', ['uses' => 'UserController@logout']);
 });
-
